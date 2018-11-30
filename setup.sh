14dc70b8049788bf7abf41bca7e1b3235bab35fb
100Rashmi <singhrashmi579@gmail.com>
2018-11-30 22:42:27 +0530

    add setup file

---
+++ setup.sh
@@ -0,0 +1,16 @@
+pip install flask-restful
+pip install pip install sqlalchemy
+pip install pymysql
+pip install requests
+pip install flask-cors
+
+
+# Install and start mysql
+sudo apt-get install mysql-server
+mysqld
+
+# Setup Mysql Database:
+CREATE DATABASE recko;
+
+#run flaskapp:
+python flaskapp.py
